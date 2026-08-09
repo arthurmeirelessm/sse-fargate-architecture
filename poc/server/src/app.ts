010432153bacac37c2060c489efb872812b56f9c
Arthur Meireles <arthurmsesjdfdn@gmail.com>
2026-08-08 21:22:05 -0300

    test gihub actios auto deploy on AWS / v5

--- poc/server/src/app.ts
+++ poc/server/src/app.ts
@@ -13,9 +13,6 @@ export interface AppConfig {
   staticDir?: string;
 }
 
-
-
-
 export const defaultConfig: AppConfig = {
   version: process.env.APP_VERSION ?? "1.0.0",
   sidecarUrl: process.env.SIDECAR_URL ?? "http://127.0.0.1:8061",
